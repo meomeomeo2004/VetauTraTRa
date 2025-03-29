2364ca1f434bc07548098f91588feaf39bbee7da
d-t-tam <dtam676767@gmail.com>
2025-03-29 19:23:38 +0700

    edit VoucherController

--- src/java/controller/customerController/VoucherController.java
+++ src/java/controller/customerController/VoucherController.java
@@ -23,15 +23,32 @@ public class VoucherController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 
-        List<Voucher> vouchers = voucherDAO.getAllVoucher();
+        int page = 1;
+        int pageSize = 10;
+
+        String pageParam = request.getParameter("page");
+        if (pageParam != null) {
+            try {
+                page = Integer.parseInt(pageParam);
+            } catch (NumberFormatException e) {
+                page = 1;
+            }
+        }
+
+        int totalVouchers = voucherDAO.getTotalVoucherCount();
+        int totalPages = (int) Math.ceil((double) totalVouchers / pageSize);
+
+        List<Voucher> vouchers = voucherDAO.getVouchersPaged(page, pageSize);
+
         request.setAttribute("vouchers", vouchers);
+        request.setAttribute("currentPage", page);
+        request.setAttribute("totalPages", totalPages);
+
         request.getRequestDispatcher("voucher.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-
     }
-
 }
